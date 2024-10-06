1f44e2f65466846463733b6162b7a20b7d40d693
surface74 <surface.msn@gmail.com>
2024-10-06 18:53:36 +0300

    feat: implement fs/copy.js

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,28 @@
-const copy = async () => {
-    // Write your code here 
+import { fileURLToPath } from 'node:url';
+import { join, dirname } from 'node:path';
+import { copyFile, mkdir, readdir } from 'node:fs/promises';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const SOURCE_FOLDER = join(__dirname, 'files');
+const DESTINATION_FOLDER = join(__dirname, 'files_copy');
+const ERR_MESSAGE = 'FS operation failed';
+
+const copy = async (source = SOURCE_FOLDER, destination = DESTINATION_FOLDER) => {
+    try {
+        await mkdir(destination, { recursive: false });
+
+        const entities = await readdir(source, { withFileTypes: true });
+
+        await Promise.all(entities.map(entity => {
+            if (entity.isDirectory()) {
+                copy(join(source, entity.name), join(destination, entity.name));
+            } else {
+                copyFile(join(source, entity.name), join(destination, entity.name));
+            }
+        }));
+    } catch (error) {
+        throw new Error(`${ERR_MESSAGE}`);
+    }
 };
 
 await copy();
