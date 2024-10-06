26c8d0be889c922351fa4fee784674043c0b5700
surface74 <surface.msn@gmail.com>
2024-10-06 22:51:51 +0300

    feat: implement hash

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,15 @@
+import { fileURLToPath } from 'node:url';
+import { dirname, join } from 'node:path';
+import { createReadStream } from 'node:fs';
+import {createHash} from 'node:crypto';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const INPUT_FILE = join(__dirname, 'files', 'fileToCalculateHashFor.txt');
+
 const calculateHash = async () => {
-    // Write your code here 
+    const hash = createHash('sha256');
+    const readStream = createReadStream(INPUT_FILE).pipe(hash).setEncoding('hex');
+    readStream.on('data', (chunk) => console.log(chunk));
 };
 
 await calculateHash();
