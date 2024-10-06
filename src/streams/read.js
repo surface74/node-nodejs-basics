abc7bbe0137e9d2242b11bb23d40388d1fdbe059
surface74 <surface.msn@gmail.com>
2024-10-06 23:45:15 +0300

    feat: implement stream/read.js

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,13 @@
+import { fileURLToPath } from 'url';
+import { dirname, join } from 'path';
+import { createReadStream } from 'node:fs';
+import { stdout, stdin } from 'node:process';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const FILE_TO_READ = join(__dirname, 'files', 'fileToRead.txt');
+
 const read = async () => {
-    // Write your code here 
+    createReadStream(FILE_TO_READ).pipe(stdout);
 };
 
 await read();
