db04df657bc7af2ee13178e40d5c12afc04afb59
surface74 <surface.msn@gmail.com>
2024-10-06 23:48:49 +0300

    feat: implement stream/write.js

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,7 +1,7 @@
 import { fileURLToPath } from 'url';
 import { dirname, join } from 'path';
 import { createReadStream } from 'node:fs';
-import { stdout, stdin } from 'node:process';
+import { stdout } from 'node:process';
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 const FILE_TO_READ = join(__dirname, 'files', 'fileToRead.txt');
