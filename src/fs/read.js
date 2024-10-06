2a8cdc082c61625daaae5787c9dfb1c3137c1792
surface74 <surface.msn@gmail.com>
2024-10-06 21:16:41 +0300

    feat: implement fs/read.js

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,18 @@
+import { fileURLToPath } from 'node:url';
+import { join, dirname } from 'node:path';
+import { readFile } from 'node:fs/promises';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const SOURCE_FILE = join(__dirname, 'files', 'fileToRead.txt');
+const ERR_MESSAGE = 'FS operation failed';
+
 const read = async () => {
-    // Write your code here 
+    try {
+        const content = await readFile(SOURCE_FILE, { encoding: 'utf8' });
+        console.log(content);
+    } catch (error) {
+        throw new Error(`${ERR_MESSAGE}`);
+    }
 };
 
 await read();
