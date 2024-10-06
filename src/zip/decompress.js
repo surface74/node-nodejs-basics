d1762983c56384f57ff89ed34602014dee295949
surface74 <surface.msn@gmail.com>
2024-10-06 23:56:52 +0300

    feat: implement zip/decompress.js

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,24 @@
+import { fileURLToPath } from 'node:url';
+import { dirname, join } from 'path';
+import { createReadStream, createWriteStream } from 'node:fs';
+import { pipeline } from 'node:stream';
+import { createUnzip } from 'node:zlib';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const SOURCE_FILE = join(__dirname, 'files', 'archive.gz');
+const DESTINATION_FILE = join(__dirname, 'files', 'fileToCompress.txt');
+
 const decompress = async () => {
-    // Write your code here 
+    const unzip = createUnzip();
+    await pipeline(
+        createReadStream(SOURCE_FILE),
+        unzip,
+        createWriteStream(DESTINATION_FILE),
+        err => {
+            if (err) {
+                console.log(err.message)
+            }
+        });
 };
 
 await decompress();
