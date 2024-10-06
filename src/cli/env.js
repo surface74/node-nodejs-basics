6bbee50257c99d49aedbcc33027365966630d6c0
surface74 <surface.msn@gmail.com>
2024-10-06 21:22:25 +0300

    feat: implement cli/env.js

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,18 @@
+import { env } from 'node:process';
+
+const PREFIX = 'RSS_';
+
 const parseEnv = () => {
-    // Write your code here 
+    const params = Object.entries(env).reduce(
+        (acc, [key, value]) => {
+            if (key.startsWith(PREFIX)) {
+                acc.push(`${key}=${value}`);
+            }
+            return acc;
+        }, [])
+        .join('; ');
+
+    console.log(params);
 };
 
 parseEnv();
