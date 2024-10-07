3f251d1829b3481db0175328b74122a75d111515
surface74 <surface.msn@gmail.com>
2024-10-07 10:22:20 +0300

    refactor: fix issue with stdout in hash/calcHash.js

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -3,13 +3,16 @@ import { dirname, join } from 'node:path';
 import { createReadStream } from 'node:fs';
 import {stdout} from 'node:process';
 import {createHash} from 'node:crypto';
+import { EOL } from 'node:os';
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 const INPUT_FILE = join(__dirname, 'files', 'fileToCalculateHashFor.txt');
 
 const calculateHash = async () => {
     const hash = createHash('sha256');
-    createReadStream(INPUT_FILE).pipe(hash).setEncoding('hex').pipe(stdout);
+
+    createReadStream(INPUT_FILE).pipe(hash).setEncoding('hex')
+        .on('data', chunk => stdout.write(chunk + EOL));
 };
 
 await calculateHash();
